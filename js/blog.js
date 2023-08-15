05465a805cdd9a73d8f1b07eb55262d011c2ae69
Batu <batuhan.yildirim.tr@gmail.com>
2023-08-14 23:11:35 -0400

    blogpage line images updated

--- js/blog.js
+++ js/blog.js
@@ -2,24 +2,21 @@ var blogPostsData = [
     {
         id: "post1",
         title: `Rhinoplasty: A New Nose, A New Confidence`,
-        description: `Every individual has their own unique sense of beauty. One way that enhances the self-confidence and improves 
-        the aesthetic appearance for many people is rhinoplasty, also known as nose sur- gery. The nose is one of the most prominent 
-        features of the face and greatly influences our over- all appearance. Rhinoplasty is a surgical procedure performed to 
-        correct the shape and size of...`,
+        description: `Every individuae way that enhances the self-confidence and improves 
+        the aesthetic appearanc`,
         buttonLabel: `More >>`,
         buttonUrl: "blogSingle.html?id=post1",
-        imageUrl: "images/insta.png"
+        imageUrl: "images/nose-dummy.jpg",
     },
     {
         id: "post2",
         title: `What to Consider When Deciding on Rhinoplasty?`,
         description: `Doing thorough research: Gather information about rhinoplasty from reliable sources. Learn about the 
-        rhinoplasty process, the recovery period, and potential risks. Conduct research to find a repu- table plastic surgeon 
-        and evaluate their references. Setting realistic expectations: Rhinoplasty is a surgical procedure aimed at improving or 
+        rhinoplasty process,  
         changing...`,
         buttonLabel: `More >>`,
         buttonUrl: "blogSingle.html?id=post2",
-        imageUrl: "images/insta.png"
+        imageUrl: "images/breast1.jpg"
     },
     {
         id: "post3",
@@ -30,7 +27,7 @@ var blogPostsData = [
         changing...`,
         buttonLabel: `More >>`,
         buttonUrl: "blogSingle.html?id=post2",
-        imageUrl: "images/insta.png"
+        imageUrl: "images/dental1.jpg"
     },
     {
         id: "post4",
@@ -41,7 +38,7 @@ var blogPostsData = [
         changing...`,
         buttonLabel: `More >>`,
         buttonUrl: "blogSingle.html?id=post2",
-        imageUrl: "images/insta.png"
+        imageUrl: "images/hair1.jpg"
     }
 ];
 
