26fd4a29068c93a8a41d6d8e47c920f5571dc2c6
Batu <batuhan.yildirim.tr@gmail.com>
2023-08-25 05:06:58 -0400

    mobile updates

--- js/landing-pageMobile.js
+++ js/landing-pageMobile.js
@@ -4,7 +4,9 @@ window.addEventListener("scroll", function () {
 
     // Calculate the desired opacity based on the scroll percentage
     const desiredOpacity = 1 - scrollPercentage * 2;
+    const desiredTop = -1000 * scrollPercentage;
 
     // Apply the calculated opacity to the video element
     document.querySelector(".video-bg").style.opacity = desiredOpacity;
+    document.querySelector(".landing-page").style.top = `${desiredTop}px`;
 });
