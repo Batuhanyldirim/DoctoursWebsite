2f3a41310507895616cf3e529f000558639c8be8
Batu <batuhan.yildirim.tr@gmail.com>
2023-08-25 18:15:44 -0400

    mobile updates

--- js/landing-pageMobile.js
+++ js/landing-pageMobile.js
@@ -11,12 +11,28 @@ window.addEventListener("scroll", function () {
     const servicesStart3 = -140;
     const servicesStart4 = -160;
 
+    // Update the background color based on the scroll percentage
+    const colorStart = 0.40;
+    const backgroundColorR = 255 - Math.round(48 * 3 * (scrollPercentage - colorStart)); // Adjust the R component
+    const backgroundColorG = 255 - Math.round(26 * 3 * (scrollPercentage - colorStart)); // Adjust the G component
+    const backgroundColorB = 255 - Math.round(26 * 3 * (scrollPercentage - colorStart)); // Adjust the B component
+    const backgroundColor = `rgb(${backgroundColorR}, ${backgroundColorG}, ${backgroundColorB})`;
+
+
     // Apply the calculated opacity to the video element
     document.querySelector(".video-bg").style.opacity = desiredOpacity;
     document.querySelector(".landing-page").style.top = `${desiredTop}px`;
 
-    console.log({ scrollPercentage })
-    console.log(servicesStart1 + serviceAnimation)
+    if (scrollPercentage > 0.4) {
+        document.querySelector(".whyDoc").style.backgroundColor = backgroundColor;
+
+        const ourServicesLinks = document.querySelectorAll(".our-services a");
+        ourServicesLinks.forEach(link => {
+            link.style.backgroundColor = backgroundColor;
+        });
+    }
+
+
     if (servicesStart1 + serviceAnimation < 0) document.querySelector(".service-container1").style.left = `${servicesStart1 + serviceAnimation}%`;
     if (servicesStart2 + serviceAnimation < 0) document.querySelector(".service-container2").style.left = `${servicesStart2 + serviceAnimation}%`;
     if (servicesStart3 + serviceAnimation < 0) document.querySelector(".service-container3").style.left = `${servicesStart3 + serviceAnimation}%`;
