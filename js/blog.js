ed793219899206f5175534532084a702811d9f4d
Batu <batuhan.yildirim.tr@gmail.com>
2023-10-14 17:22:43 +0300

    update

--- js/blog.js
+++ js/blog.js
@@ -4,7 +4,7 @@ var blogPostsData = [
         title: `What is facial aesthetics?`,
         description: ``,
         buttonLabel: `More >>`,
-        buttonUrl: "blogSingleMobile.html?id=post1",
+        buttonUrl: "blogSingle.html?id=post1",
         imageUrls: "images/foto001-2.jpg",
     },
     {
@@ -12,7 +12,7 @@ var blogPostsData = [
         title: `What is a Botox injection?`,
         description: ``,
         buttonLabel: `More >>`,
-        buttonUrl: "blogSingleMobile.html?id=post2",
+        buttonUrl: "blogSingle.html?id=post2",
         imageUrls: "images/botox2.jpg"
     },
     {
@@ -20,7 +20,7 @@ var blogPostsData = [
         title: `Learn More About Chemical Peeling`,
         description: ``,
         buttonLabel: `More >>`,
-        buttonUrl: "blogSingleMobile.html?id=post3",
+        buttonUrl: "blogSingle.html?id=post3",
         imageUrls: "images/pexels-polina-kovaleva-6543489.jpg"
     },
     {
@@ -28,7 +28,7 @@ var blogPostsData = [
         title: `Blepharoplasty (Eyelid Aesthetics)`,
         description: ``,
         buttonLabel: `More >>`,
-        buttonUrl: "blogSingleMobile.html?id=post4",
+        buttonUrl: "blogSingle.html?id=post4",
         imageUrls: "images/sagging eyelid.png"
     },
     {
@@ -36,7 +36,7 @@ var blogPostsData = [
         title: `A New Nose, A New Confidence`,
         description: ``,
         buttonLabel: `More >>`,
-        buttonUrl: "blogSingleMobile.html?id=post5",
+        buttonUrl: "blogSingle.html?id=post5",
         imageUrls: "images/nose-dummy.jpg"
     },
     {
@@ -44,7 +44,7 @@ var blogPostsData = [
         title: `Face and Forehead Lift (French Lift)`,
         description: ``,
         buttonLabel: `More >>`,
-        buttonUrl: "blogSingleMobile.html?id=post6",
+        buttonUrl: "blogSingle.html?id=post6",
         imageUrls: "images/nose-dummy.jpg"
     },
     {
@@ -52,7 +52,7 @@ var blogPostsData = [
         title: `What is Dental Implant Treatment?`,
         description: ``,
         buttonLabel: `More >>`,
-        buttonUrl: "blogSingleMobile.html?id=post7",
+        buttonUrl: "blogSingle.html?id=post7",
         imageUrls: "images/teeth-aesth1.jpg"
     },
     {
@@ -60,7 +60,7 @@ var blogPostsData = [
         title: `Dermal Filler`,
         description: ``,
         buttonLabel: `More >>`,
-        buttonUrl: "blogSingleMobile.html?id=post8",
+        buttonUrl: "blogSingle.html?id=post8",
         imageUrls: "images/skin.jpg"
     },
     {
@@ -68,7 +68,7 @@ var blogPostsData = [
         title: `In-vitro fertilization (IVF)`,
         description: ``,
         buttonLabel: `More >>`,
-        buttonUrl: "blogSingleMobile.html?id=post9",
+        buttonUrl: "blogSingle.html?id=post9",
         imageUrls: "images/baby.jpg"
     },
     {
@@ -76,7 +76,7 @@ var blogPostsData = [
         title: `Learn More About Liposuction`,
         description: ``,
         buttonLabel: `More >>`,
-        buttonUrl: "blogSingleMobile.html?id=post10",
+        buttonUrl: "blogSingle.html?id=post10",
         imageUrls: "images/liposux2.jpg"
     },
     {
@@ -84,7 +84,7 @@ var blogPostsData = [
         title: `What are Arm and Leg Lift Surgeries`,
         description: ``,
         buttonLabel: `More >>`,
-        buttonUrl: "blogSingleMobile.html?id=post11",
+        buttonUrl: "blogSingle.html?id=post11",
         imageUrls: "images/armlift.jpg"
     },
     {
@@ -92,7 +92,7 @@ var blogPostsData = [
         title: `Otoplasty`,
         description: ``,
         buttonLabel: `More >>`,
-        buttonUrl: "blogSingleMobile.html?id=post12",
+        buttonUrl: "blogSingle.html?id=post12",
         imageUrls: "images/cauliflower-ear1.jpg"
     },
     {
@@ -100,7 +100,7 @@ var blogPostsData = [
         title: `TEETH WHITENING`,
         description: ``,
         buttonLabel: `More >>`,
-        buttonUrl: "blogSingleMobile.html?id=post13",
+        buttonUrl: "blogSingle.html?id=post13",
         imageUrls: "images/teeth-aesth1.jpg"
     },
     {
@@ -108,7 +108,7 @@ var blogPostsData = [
         title: `ORTHODONTY`,
         description: ``,
         buttonLabel: `More >>`,
-        buttonUrl: "blogSingleMobile.html?id=post14",
+        buttonUrl: "blogSingle.html?id=post14",
         imageUrls: "images/ortho4.png"
     },
     {
@@ -116,7 +116,7 @@ var blogPostsData = [
         title: `PERIDONTOLOGY`,
         description: ``,
         buttonLabel: `More >>`,
-        buttonUrl: "blogSingleMobile.html?id=post15",
+        buttonUrl: "blogSingle.html?id=post15",
         imageUrls: "images/periodont1.png"
     },
     {
@@ -124,7 +124,7 @@ var blogPostsData = [
         title: `ORAL ODORS`,
         description: ``,
         buttonLabel: `More >>`,
-        buttonUrl: "blogSingleMobile.html?id=post16",
+        buttonUrl: "blogSingle.html?id=post16",
         imageUrls: "images/halitosis3.png"
     },
     {
@@ -132,7 +132,7 @@ var blogPostsData = [
         title: `DENTAL IMPLANT`,
         description: ``,
         buttonLabel: `More >>`,
-        buttonUrl: "blogSingleMobile.html?id=post17",
+        buttonUrl: "blogSingle.html?id=post17",
         imageUrls: "images/dental-implant5.png"
     },
     {
@@ -140,7 +140,7 @@ var blogPostsData = [
         title: `ENDODONTICS`,
         description: ``,
         buttonLabel: `More >>`,
-        buttonUrl: "blogSingleMobile.html?id=post17",
+        buttonUrl: "blogSingle.html?id=post17",
         imageUrls: "images/endodontics2.png"
     },
     {
@@ -148,7 +148,7 @@ var blogPostsData = [
         title: `Liposuction`,
         description: ``,
         buttonLabel: `More >>`,
-        buttonUrl: "blogSingleMobile.html?id=post19",
+        buttonUrl: "blogSingle.html?id=post19",
         imageUrls: "images/liposux2.jpg"
     },
     {
@@ -156,7 +156,7 @@ var blogPostsData = [
         title: `Facial Aesthetics`,
         description: ``,
         buttonLabel: `More >>`,
-        buttonUrl: "blogSingleMobile.html?id=post20",
+        buttonUrl: "blogSingle.html?id=post20",
         imageUrls: "images/foto001-2.jpg"
     },
     {
@@ -164,7 +164,7 @@ var blogPostsData = [
         title: `Abdominoplasty`,
         description: ``,
         buttonLabel: `More >>`,
-        buttonUrl: "blogSingleMobile.html?id=post21",
+        buttonUrl: "blogSingle.html?id=post21",
         imageUrls: "images/tummy2.png"
     },
     {
@@ -172,7 +172,7 @@ var blogPostsData = [
         title: `Arm-Leg Lift Surgery`,
         description: ``,
         buttonLabel: `More >>`,
-        buttonUrl: "blogSingleMobile.html?id=post22",
+        buttonUrl: "blogSingle.html?id=post22",
         imageUrls: "images/armlift.jpg"
     },
 ];
