d5fdfd9b55640ddca5f588a0a4d13e5fa5d5a14e
Batu <batuhan.yildirim.tr@gmail.com>
2023-10-16 13:29:11 +0300

    Missing blog descriptions has added to desktop

--- js/blog.js
+++ js/blog.js
@@ -2,7 +2,9 @@ var blogPostsData = [
     {
         id: "post1",
         title: `What is facial aesthetics?`,
-        description: ``,
+        description: `Facial aesthetics encompasses various services such as facelift, eyelid surgery, forehead lift, 
+        facial fillers, Botox injections, chin augmentation, and ear surgery. Facial aesthetics is the general term for surgical or non-surgical procedures performed to correct 
+        aesthetic issues in the facial area and improve the overall appearance of the face.`,
         buttonLabel: `More >>`,
         buttonUrl: "blogSingle.html?id=post1",
         imageUrls: "images/foto001-2.jpg",
@@ -10,7 +12,9 @@ var blogPostsData = [
     {
         id: "post2",
         title: `What is a Botox injection?`,
-        description: ``,
+        description: `A Botox injection is a cosmetic treatment method that uses a protein called botulinum toxin type A. 
+        Botulinum toxin temporarily relaxes muscles by blocking nerve signals from nerves to muscles. Botox injections are used to reduce wrinkles and fine lines on the face. They are most commonly applied to 
+        the forehead, frown lines between the eyebrows, crow's feet, and eyebrow areas.`,
         buttonLabel: `More >>`,
         buttonUrl: "blogSingle.html?id=post2",
         imageUrls: "images/botox2.jpg"
@@ -18,7 +22,9 @@ var blogPostsData = [
     {
         id: "post3",
         title: `Learn More About Chemical Peeling`,
-        description: ``,
+        description: `Chemical peel is a cosmetic treatment method that involves controlled exfoliation of the skin's outer layer (epidermis) 
+        using a chemical solution. This procedure aims to rejuvenate the skin by improving skin tone, reducing pigmentation, 
+        lightening acne scars, smoothing fine wrinkles, and addressing sun damage and other skin issues.`,
         buttonLabel: `More >>`,
         buttonUrl: "blogSingle.html?id=post3",
         imageUrls: "images/pexels-polina-kovaleva-6543489.jpg"
@@ -26,7 +32,8 @@ var blogPostsData = [
     {
         id: "post4",
         title: `Blepharoplasty (Eyelid Aesthetics)`,
-        description: ``,
+        description: `Eyelids may sag and loosen during the aging process, resulting in a tired, older, and melancholic appearance.
+        Through surgery, excess skin, fat, and muscle tissue are removed to give the eyelids a more youthful, vibrant, and dynamic appearance.`,
         buttonLabel: `More >>`,
         buttonUrl: "blogSingle.html?id=post4",
         imageUrls: "images/sagging eyelid.png"
@@ -34,7 +41,8 @@ var blogPostsData = [
     {
         id: "post5",
         title: `A New Nose, A New Confidence`,
-        description: ``,
+        description: `Every individual has their own unique sense of beauty. For many people, one way to enhance their self-confidence and 
+        improve their aesthetic appearance is through rhinoplasty, also known as a nose job. `,
         buttonLabel: `More >>`,
         buttonUrl: "blogSingle.html?id=post5",
         imageUrls: "images/nose-dummy.jpg"
@@ -42,7 +50,9 @@ var blogPostsData = [
     {
         id: "post6",
         title: `Face and Forehead Lift (French Lift)`,
-        description: ``,
+        description: `Face and forehead lift is a surgical procedure performed to correct the signs of aging in the face and forehead region. 
+        The face lift procedure aims to reposition the sagging skin and muscle tissues to achieve a younger 
+        appearance, while the forehead lift targets wrinkles and sagging in the forehead.`,
         buttonLabel: `More >>`,
         buttonUrl: "blogSingle.html?id=post6",
         imageUrls: "images/nose-dummy.jpg"
@@ -50,7 +60,8 @@ var blogPostsData = [
     {
         id: "post7",
         title: `What is Dental Implant Treatment?`,
-        description: ``,
+        description: `Dental implant treatment involves the placement of an artificial tooth root to replace a missing tooth. It is performed 
+        to restore a lost tooth both aesthetically and functionally.`,
         buttonLabel: `More >>`,
         buttonUrl: "blogSingle.html?id=post7",
         imageUrls: "images/teeth-aesth1.jpg"
@@ -58,7 +69,9 @@ var blogPostsData = [
     {
         id: "post8",
         title: `Dermal Filler`,
-        description: ``,
+        description: `Dermal filler is a cosmetic treatment involving injections under the skin. This treatment is used to correct 
+        wrinkles on the skin, enhance facial contours, or restore volume to areas experiencing loss. Dermal filler substances are typically made of biologically compatible materials such as hyaluronic acid, 
+        calcium hydroxylapatite, or polylactic acid.`,
         buttonLabel: `More >>`,
         buttonUrl: "blogSingle.html?id=post8",
         imageUrls: "images/skin.jpg"
@@ -74,7 +87,9 @@ var blogPostsData = [
     {
         id: "post10",
         title: `Learn More About Liposuction`,
-        description: ``,
+        description: `Liposuction is a surgical procedure performed to remove excess fat tissue from the body. It involves using a vacuum ,
+        device and a thin cannula to suction out fat cells. This reduces unwanted fat deposits and reshapes the body. Liposuction can be applied to areas such as the abdomen, waist, hips, thighs, knees, upper arms, neck, and chin. It is 
+        commonly chosen to reduce excess fat deposits in these areas and improve body contours`,
         buttonLabel: `More >>`,
         buttonUrl: "blogSingle.html?id=post10",
         imageUrls: "images/liposux2.jpg"
@@ -82,7 +97,10 @@ var blogPostsData = [
     {
         id: "post11",
         title: `What are Arm and Leg Lift Surgeries`,
-        description: ``,
+        description: `Arm-leg lift surgery" typically refers to a surgical procedure performed to correct skin sagging caused by bariatric
+        surgery or aging. Bariatric surgery involves surgical interventions aimed at weight loss for individuals who are 
+        severely overweight or obese. Significant weight loss resulting from such surgeries can lead to substantial skin 
+        sagging. Skin sagging is particularly common in the arm, leg, and abdominal areas.`,
         buttonLabel: `More >>`,
         buttonUrl: "blogSingle.html?id=post11",
         imageUrls: "images/armlift.jpg"
@@ -90,7 +108,11 @@ var blogPostsData = [
     {
         id: "post12",
         title: `Otoplasty`,
-        description: ``,
+        description: `Otoplasty is a surgical procedure performed to correct abnormalities in the shape or position of the outer ear, 
+        either congenital or resulting from trauma. Otoplasty is commonly performed on individuals with issues such as 
+        "protruding ears" or "lop ears." The purpose of otoplasty is to bring the ear into a more symmetrical position, prevent ears from protruding too far 
+        from the head, or correct abnormal folds or deformities in the ear's shape. During the procedure, the surgeon typically 
+        makes an incision behind the ear to access the ear cartilage.`,
         buttonLabel: `More >>`,
         buttonUrl: "blogSingle.html?id=post12",
         imageUrls: "images/cauliflower-ear1.jpg"
@@ -98,7 +120,8 @@ var blogPostsData = [
     {
         id: "post13",
         title: `TEETH WHITENING`,
-        description: ``,
+        description: `Teeth Whitening, dental cosmetic procedure performed to lighten the color of teeth and achieve a brighter appearance. Here are the commonly 
+        used methods of teeth whitening: At-Home Teeth Whitening Kits, In-Office Teeth Whitening, Veneers, Dental Crowns, `,
         buttonLabel: `More >>`,
         buttonUrl: "blogSingle.html?id=post13",
         imageUrls: "images/teeth-aesth1.jpg"
@@ -106,7 +129,9 @@ var blogPostsData = [
     {
         id: "post14",
         title: `ORTHODONTY`,
-        description: ``,
+        description: `Orthodontic treatment typically yields the best results during the adolescent period, as bones are still 
+        growing and teeth are more mobile during this phase. However, there are also orthodontic treatment options 
+        available for adults.`,
         buttonLabel: `More >>`,
         buttonUrl: "blogSingle.html?id=post14",
         imageUrls: "images/ortho4.png"
@@ -114,7 +139,10 @@ var blogPostsData = [
     {
         id: "post15",
         title: `PERIDONTOLOGY`,
-        description: ``,
+        description: `The most common periodontal diseases are gum diseases. These are divided into two main categories: gingivitis, which is 
+        inflammation of the gums, which is the advanced form of gum inflammation. These diseases can 
+        develop due to various factors such as poor oral hygiene, genetic factors, smoking, systemic diseases like diabetes, 
+        hormonal changes, and stress.`,
         buttonLabel: `More >>`,
         buttonUrl: "blogSingle.html?id=post15",
         imageUrls: "images/periodont1.png"
@@ -122,7 +150,9 @@ var blogPostsData = [
     {
         id: "post16",
         title: `ORAL ODORS`,
-        description: ``,
+        description: `Halitosis, also known as bad breath, is a condition characterized by an unpleasant odor emanating from the mouth that 
+        is persistent or recurrent. It is often caused by oral and dental health issues. These include poor oral hygiene, tooth 
+        decay, gum diseases, bacteria buildup on the tongue, dry mouth, throat infections, and other oral conditions.`,
         buttonLabel: `More >>`,
         buttonUrl: "blogSingle.html?id=post16",
         imageUrls: "images/halitosis3.png"
@@ -130,7 +160,9 @@ var blogPostsData = [
     {
         id: "post17",
         title: `DENTAL IMPLANT`,
-        description: ``,
+        description: `Dental implants can be an ideal solution for individuals seeking to enhance their aesthetic appearance and address chewing 
+        function and speech difficulties caused by missing teeth. However, suitability and risks should be carefully evaluated for 
+        each patient. The dental implant procedure is typically performed by an experienced dentist or implant surgeon.`,
         buttonLabel: `More >>`,
         buttonUrl: "blogSingle.html?id=post17",
         imageUrls: "images/dental-implant5.png"
@@ -138,7 +170,10 @@ var blogPostsData = [
     {
         id: "post18",
         title: `ENDODONTICS`,
-        description: ``,
+        description: `Endodontics is a branch of dentistry that deals with the treatment of the pulp tissue inside the 
+        teeth. It involves diagnosing and treating conditions affecting the dental pulp, such as inflammation, infection, or damage.
+        Endodontic treatment is typically applied in the following situations: Deep cavities, Dental trauma, Dental abscesses, Tooth fractures, 
+        `,
         buttonLabel: `More >>`,
         buttonUrl: "blogSingle.html?id=post17",
         imageUrls: "images/endodontics2.png"
@@ -146,7 +181,10 @@ var blogPostsData = [
     {
         id: "post19",
         title: `Liposuction`,
-        description: ``,
+        description: `Liposuction is a surgical procedure performed to remove excess fat tissue from the body. It involves using a vacuum 
+        device and a thin cannula to suction out fat cells. This reduces unwanted fat deposits and reshapes the body. Liposuction can be applied to areas such as the abdomen, waist, hips, 
+        thighs, knees, upper arms, neck, and chin. It is commonly chosen to reduce excess fat deposits in these areas and 
+        improve body contours.`,
         buttonLabel: `More >>`,
         buttonUrl: "blogSingle.html?id=post19",
         imageUrls: "images/liposux2.jpg"
@@ -154,7 +192,10 @@ var blogPostsData = [
     {
         id: "post20",
         title: `Facial Aesthetics`,
-        description: ``,
+        description: `Facial aesthetics is a medical field that encompasses surgical or non-surgical procedures aimed at improving the 
+        appearance and shape of the face. These procedures are performed to correct, balance, or rejuvenate the aesthetic 
+        appearance of an individual's face. Facial aesthetics encompasses various services such as facelift, eyelid surgery, 
+        forehead lift, facial fillers, Botox injections, chin augmentation, and ear surgery.`,
         buttonLabel: `More >>`,
         buttonUrl: "blogSingle.html?id=post20",
         imageUrls: "images/foto001-2.jpg"
@@ -162,7 +203,9 @@ var blogPostsData = [
     {
         id: "post21",
         title: `Abdominoplasty`,
-        description: ``,
+        description: `Abdominoplasty is an effective surgical procedure that corrects skin sagging and tightens the abdominal area. 
+        However, as with any surgical procedure, it's important to discuss risks and the recovery process in detail 
+        with our doctor.`,
         buttonLabel: `More >>`,
         buttonUrl: "blogSingle.html?id=post21",
         imageUrls: "images/tummy2.png"
@@ -170,7 +213,10 @@ var blogPostsData = [
     {
         id: "post22",
         title: `Arm-Leg Lift Surgery`,
-        description: ``,
+        description: `Arm-leg lift surgery" typically refers to a surgical procedure performed to correct skin sagging caused by bariatric 
+        surgery or aging. Bariatric surgery involves surgical interventions aimed at weight loss for individuals who are 
+        severely overweight or obese. Significant weight loss resulting from such surgeries can lead to substantial skin 
+        sagging.`,
         buttonLabel: `More >>`,
         buttonUrl: "blogSingle.html?id=post22",
         imageUrls: "images/armlift.jpg"
